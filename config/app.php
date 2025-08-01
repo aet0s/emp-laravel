04d46ff0e138afdbaefdff02de5ea6ea91255da2
Hardick B. <hardick@solarman.in>
2025-08-01 17:02:56 +0530

    sed

--- config/app.php
+++ config/app.php
@@ -4,7 +4,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Application Name
+    | Application Name is hardick
     |--------------------------------------------------------------------------
     |
     | This value is the name of your application, which will be used when the
